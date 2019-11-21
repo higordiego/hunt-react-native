8616ba99c038f4c15aaa2f252a35ff9bb0d97eff
Higor Diego <higordiegoti@gmail.com>
2019-11-21 19:34:27 -0300

    first

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Routes from './routes';
+
+// import { WebView } from 'react-native-webview';
+
+const App = () => <Routes />;
+// const App = () => <WebView source={{ uri: 'https://entrega.centromedicocory.com.br/entrega/login' }} />
+
+export default App;
+
+
+// `https://rocketseat-node.herokuapp.com/api/products`
